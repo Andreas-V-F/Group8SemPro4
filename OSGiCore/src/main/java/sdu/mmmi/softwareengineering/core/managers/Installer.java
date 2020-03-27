86a9e1acbaa9520228bcff5d920bd79b6f00d092
Andreas Frederiksen <Andreas020299@gmail.com>
2020-03-27 10:59:29 +0100

    OSGi måske

---
+++ OSGiCore/src/main/java/sdu/mmmi/softwareengineering/core/managers/Installer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sdu.mmmi.softwareengineering.core.managers;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.openide.modules.ModuleInstall;
+
+public class Installer extends ModuleInstall {
+
+    private static Game g;
+
+    @Override
+    public void restored() {
+
+        g = new Game();
+
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "Jølp Eternal";
+        cfg.width = 800;
+        cfg.height = 600;
+        cfg.useGL30 = false;
+        cfg.resizable = false;
+//        cfg.pauseWhenBackground = true;
+//        cfg.pauseWhenMinimized = true;
+
+        new LwjglApplication(g, cfg);
+    }
+}
