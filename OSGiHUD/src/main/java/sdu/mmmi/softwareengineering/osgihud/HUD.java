878362f3c9f9cc42ae4eef440d3105966e6985e6
Mikkel Høyberg <43138794+mikk3l1@users.noreply.github.com>
2020-04-08 20:18:21 +0200

    Created HUD module
    Created HUD module.
    Added default texture pictures to be used if an entity don't have or missing a texture
    removed the "BossMonster" texture

---
+++ OSGiHUD/src/main/java/sdu/mmmi/softwareengineering/osgihud/HUD.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sdu.mmmi.softwareengineering.osgihud;
+
+import sdu.mmmi.softwareengineering.osgicommon.data.Entity;
+
+/**
+ *
+ * @author Mikkel Høyberg
+ */
+public class HUD extends Entity {
+
+}
---
+++ OSGiHUD/src/main/java/sdu/mmmi/softwareengineering/osgihud/HUDProcessor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sdu.mmmi.softwareengineering.osgihud;
+
+import sdu.mmmi.softwareengineering.osgicommon.data.GameData;
+import sdu.mmmi.softwareengineering.osgicommon.data.World;
+import sdu.mmmi.softwareengineering.osgicommon.services.IEntityProcessingService;
+
+/**
+ *
+ * @author Mikkel Høyberg
+ */
+public class HUDProcessor implements IEntityProcessingService {
+
+    @Override
+    public void process(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
